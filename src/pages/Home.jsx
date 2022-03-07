16b4a7fafa014c4b9af4e7999737778adc287f29
brytebee <brytebee@gmail.com>
2022-03-07 15:45:07 +0100

    :white_check_mark: Update Home page
    Updated hoome page to fetch data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCountriesFromAPI } from '../redux/countries/countries';
 
-const Home = () => <div>Home</div>;
+const Home = () => {
+  const dispatch = useDispatch();
+  const allCountries = useSelector((state) => state.countries);
+
+  useEffect(() => {
+    dispatch(getCountriesFromAPI());
+  }, []);
+  return (
+    <div>
+      {allCountries.map((country) => (
+        <p>{country}</p>
+      ))}
+    </div>
+  );
+};
+
 export default Home;
