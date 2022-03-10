819450376953213708b52f6de658ae63ba447c79
brytebee <brytebee@gmail.com>
2022-03-10 22:46:56 +0100

    :lipstick: style 404
    Styled the 404 page

--- src/pages/FourOhFour.jsx
+++ src/pages/FourOhFour.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const img = 'https://i.pinimg.com/originals/0e/c0/db/0ec0dbf1e9a008acb9955d3246970e15.gif';
 
 const FourOhFour = () => (
-  <div>
+  <div className="four-oh-four">
     <h2>Oh Oh!</h2>
     <p>The page you are trying to access is unavailable at the moment...</p>
-    <img className="four-oh-four" src={img} alt="Error dispaly" />
+    <img className="four-oh-four-img" src={img} alt="Error dispaly" />
   </div>
 );
 
